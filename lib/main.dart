ab84c1ba43a1263496ccabb60fa47ba8f97433bb
tri angga utama <trianggategarutama@gmail.com>
2020-02-23 11:12:42 +0800

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi flutter pertama saya"),
-        ),
-        body: Center(
-            child: Container(
-                color: Colors.deepPurpleAccent,
-                width: 150,
-                height: 50,
-                padding: EdgeInsets.all(15),
-                child: Text("Hallo Semua",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 18)))),
-      ),
+      home :Scaffold(
+        appBar : AppBar(title :Text("latihan row and column"),),
+        body   : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("menu 1"),
+            Text("menu 2"),
+            Text("menu 3"),
+            Row(
+              children: <Widget>[
+                Text("Sub Menu 1"),
+                Text("Sub Menu 2")
+              ],
+            )
+          ],
+        ) ,
+      ) ,
     );
   }
 }
