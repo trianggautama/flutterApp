efb071356a09272c09c256825240927ff319f7ff
tri angga utama <trianggategarutama@gmail.com>
2020-02-24 21:44:04 +0800

    anonymous method

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "ini adalah state";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Latihan Container and padiing margin"),
-      ),
-      body: Container(
-        color: Colors.white,
-        margin: EdgeInsets.all(5),
-        child: Container(
-            margin: EdgeInsets.all(25),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius : BorderRadius.circular(20),
-              color : Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 25.0, // soften the shadow
-                  spreadRadius: 3.0, //extend the shadow
-                  offset: Offset(
-                    5.0, // Move to right 10  horizontally
-                    5.0, // Move to bottom 10 Vertically
-                  ),
-                )
-                ],
+            appBar: AppBar(
+              title: Text("Anonymous Method"),
             ),
-            height:300,
-            width:300,
-            child:Text("Holla", style: TextStyle(color: Colors.blue, fontSize: 32, fontWeight: FontWeight.w500))),
-      ),
-    ));
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(message),
+                RaisedButton(
+                  child: Text("Click Me"),
+                  onPressed: (){
+                    setState(() {
+                    message = "tombol sudah ditekan";
+                    });
+                  },
+                )
+              ],
+            ))));
   }
 }
