5b28912f2df7a1fab99b6e061765fd711a0608a8
tri angga utama <trianggategarutama@gmail.com>
2020-02-25 11:47:27 +0800

    List and ListView

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String message = "ini adalah state";
+  List<Widget> widgets = [];
+  int counter = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("Anonymous Method"),
-            ),
-            body: Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(message),
-                RaisedButton(
-                  color: Colors.blue,
-                  child: Text("Click Me",style: TextStyle(color: Colors.white)),
-                  onPressed: (){
-                    setState(() {
-                    message = "tombol sudah ditekan";
-                    });
-                  },
-                )
-              ],
-            ))));
+        appBar: AppBar(
+        title: Text("Latihan List"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(child: Text(" Tambah Data "), onPressed: (){
+               setState(() {
+                  widgets.add(Text("Data ke -" + counter.toString()));
+                  counter++;
+               });
+              }),
+              RaisedButton(child: Text(" Hapus Data"))
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widgets
+          )
+        ],
+    )
+    )
+    );
   }
 }
